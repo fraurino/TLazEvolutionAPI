b800bc2b2b1fb2f011515c2921c875c5f6cbfa26
iNovefast <77357290+iNovefast@users.noreply.github.com>
2024-11-29 16:52:32 -0300

    Fix: Removido unit png.

--- src/formqrcode.pas
+++ src/formqrcode.pas
@@ -5,7 +5,7 @@ unit FormQrCode;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls, png;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
 
 type
 
